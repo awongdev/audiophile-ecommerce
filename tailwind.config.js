56c7f36a3d54ce09d65f3732811f46d3cc4f9528
Aaron W <26586148+AceDub@users.noreply.github.com>
2023-04-17 14:20:47 -0700

    Chore: Add custom themes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-orange-900': 'hsl(22deg, 65%, 57%)',
+        'clr-orange-700': 'hsl(21deg, 94%, 75%)',
+        'clr-black-900': 'hsl(0deg, 0%, 0%)',
+        'clr-black-850': 'hsl(0deg, 0%, 6%)',
+        'clr-white-150': 'hsl(0deg, 0%, 95%)',
+        'clr-white-100': 'hsl(0deg, 0%, 98%)',
+        'clr-white-50': 'hsl(0deg, 0%, 100%)',
+      },
+      maxWidth: {
+        1110: '69.375rem',
+      },
+    },
   },
   plugins: [],
 };
