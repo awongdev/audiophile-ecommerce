b5d1d38bab52c4d63ede4a4c232346388e13c5ee
Aaron W <26586148+acedub@users.noreply.github.com>
2023-04-20 12:35:09 -0700

    Fix: Max width for categories menu

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -30,29 +30,31 @@ const category = [
 
 const Categories = () => {
   return (
-    <div className="mx-6 flex flex-col items-center justify-center gap-16 md:mx-10 md:flex-row md:gap-3">
-      {category.map(({ category, img, to, id, description }) => (
-        <div
-          key={id}
-          className="relative flex min-h-[10.313rem] w-full max-w-[21.875rem] flex-col items-center justify-end gap-4 rounded-lg bg-clr-white-150 pb-6 lg:min-h-[12.75rem] lg:pb-[1.875rem]"
-        >
-          <Image
-            src={img}
-            alt={description}
-            className="absolute -top-1/4 max-h-[8.375rem] object-contain lg:max-h-[10.25rem]"
-          />
-          <h2 className="text-15px font-bold tracking-[1.07px] text-clr-black-900">
-            {category}
-          </h2>
-          <Link
-            href={to}
-            className="flex items-center p-1 text-[0.813rem] font-bold"
+    <div className="flex justify-center">
+      <div className="flex w-full max-w-1110 flex-col items-center gap-16 md:flex-row md:justify-between md:gap-3">
+        {category.map(({ category, img, to, id, description }) => (
+          <div
+            key={id}
+            className="relative flex min-h-[10.313rem] w-full max-w-[21.875rem] flex-col items-center justify-end gap-4 rounded-lg bg-clr-white-150 pb-6 lg:min-h-[12.75rem] lg:pb-[1.875rem]"
           >
-            <span className="pr-3 leading-none opacity-50">SHOP</span>
-            <span className="content-arrow"></span>
-          </Link>
-        </div>
-      ))}
+            <Image
+              src={img}
+              alt={description}
+              className="absolute -top-1/4 max-h-[8.375rem] object-contain lg:max-h-[10.25rem]"
+            />
+            <h2 className="text-15px font-bold tracking-[1.07px] text-clr-black-900">
+              {category}
+            </h2>
+            <Link
+              href={to}
+              className="flex items-center p-1 text-[0.813rem] font-bold"
+            >
+              <span className="pr-3 leading-none opacity-50">SHOP</span>
+              <span className="content-arrow"></span>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -7,7 +7,7 @@ interface Props {
 const MobileNav = ({ setIsMenuOpen }: Props) => {
   return (
     <div className="absolute w-full lg:hidden">
-      <div className="relative z-30 rounded-b-[8px] bg-clr-white-50 pb-9 pt-20 md:pt-24">
+      <div className="relative z-30 rounded-b-[8px] bg-clr-white-50 px-6 pb-9 pt-20 md:px-10 md:pt-24">
         <Categories />
       </div>
 
