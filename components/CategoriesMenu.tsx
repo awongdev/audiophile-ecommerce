714778b463440fc30aeeb03933da8d881db65168
Aaron W <26586148+AceDub@users.noreply.github.com>
2023-05-11 19:48:21 -0700

    Fix: CategoriesMenu clickable area to include background

--- components/CategoriesMenu.tsx
+++ components/CategoriesMenu.tsx
@@ -34,26 +34,24 @@ const Categories = () => {
     <div className="flex justify-center">
       <div className="flex w-full max-w-1110 flex-col items-center gap-16 sm:flex-row sm:gap-3 md:justify-between">
         {category.map(({ category, img, to, id, description }) => (
-          <div
+          <Link
+            href={to}
             key={id}
-            className="relative flex min-h-[10.313rem] w-full  flex-col items-center justify-end gap-4 rounded-lg bg-clr-white-150 pb-6 lg:min-h-[12.75rem] lg:pb-[1.875rem]"
+            className="relative flex min-h-[10.313rem] w-full flex-col items-center justify-end gap-4 rounded-lg bg-clr-white-150 pb-6 text-clr-black-900/50 transition-colors duration-200 hover:text-clr-orange-900 lg:min-h-[12.75rem] lg:pb-[1.875rem]"
           >
             <Image
               src={img}
               alt={description}
-              className="absolute -top-1/4 max-h-[8.375rem] object-contain lg:max-h-[10.25rem]"
+              className="absolute -top-1/4 max-h-[8.375rem] max-w-[170px] object-contain lg:max-h-[10.25rem]"
             />
             <h2 className="text-15px font-bold tracking-[1.07px] text-clr-black-900">
               {category}
             </h2>
-            <Link
-              href={to}
-              className="flex items-center p-1 text-[0.813rem] font-bold"
-            >
-              <span className="pr-3 leading-none opacity-50">SHOP</span>
+            <div className="flex items-center p-1 text-[0.813rem] font-bold">
+              <span className="pr-3 leading-none">SHOP</span>
               <Arrow />
-            </Link>
-          </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
