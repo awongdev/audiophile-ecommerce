b613227e5e5877951832fdc3b2135f7bfbceaee7
Aaron W <26586148+AceDub@users.noreply.github.com>
2023-06-08 13:47:12 -0700

    Fix: Redux-persist to createNoopStorage for server side rendering

--- redux/store.ts
+++ redux/store.ts
@@ -4,9 +4,34 @@ import paymentMethodReducer from './features/paymentMethodSlice';
 import itemQuantityReducer from './features/itemQuantitySlice';
 import cartModalReducer from './features/cartModalSlice';
 import localCartReducer from './features/localCartSlice';
-import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+import { WebStorage } from 'redux-persist/lib/types';
+
+const createNoopStorage = (): WebStorage => {
+  const isServer = typeof window === 'undefined';
+
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage('local');
+};
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
 
 const persistConfig = {
   key: 'root',
